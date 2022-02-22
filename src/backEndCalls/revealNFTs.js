6f12ce79c9d9c7dacadfb1338b1c2351e7ef0a23
Ak <akalankapathirage22@gmail.com>
2022-02-22 18:19:55 +0530

    error Handling logic revealNFTs

--- src/backEndCalls/revealNFTs.js
+++ src/backEndCalls/revealNFTs.js
@@ -1,17 +1,21 @@
 const revealNFTs = (setFeedback, blockchain, CONFIG) => {
-  console.log("smartcon", blockchain);
-  let gasLimit = CONFIG.GAS_LIMIT;
-  blockchain.smartContract.methods
-    .reveal()
-    .send({
-      gasLimit: String(gasLimit),
-      from: blockchain.account,
-    })
-    .then((receipt) => {
-      console.log(receipt);
-      setFeedback(`You Revealed It!.`);
-    })
-    .catch((err) => console.log("Error When Revealing", err));
+  if (blockchain.smartContract) {
+    let gasLimit = CONFIG.GAS_LIMIT;
+    blockchain.smartContract.methods
+      .reveal()
+      .send({
+        gasLimit: String(gasLimit),
+        from: blockchain.account,
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        setFeedback(`You Revealed It!.`);
+      })
+      .catch((err) => console.log("Error When Revealing", err));
+  } else {
+    console.error("Trying to call contract while not connected to the BlockChain.");
+    return;
+  }
 };
 
 export default revealNFTs;
