16b9f13bc4f7165eb3d3fd02488c23695e4ca685
Ak <akalankapathirage22@gmail.com>
2022-02-23 18:59:54 +0530

    rollback - removed countdown

--- src/components/StickyNotification/StickyNotification.js
+++ src/components/StickyNotification/StickyNotification.js
@@ -1,71 +1,14 @@
-import { useState, useEffect } from "react";
-import { calculateTimeLeft } from "../../utils/calculateTimeLeft";
 import "./stickyNotification.css";
 
 const StickyNotification = () => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-
-    // return () => clearTimeout(timer);
-  });
-
-  const timerComponents = [];
-  let phrase;
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    // if (timeLeft[interval] === 1) {
-    //   phrase = interval.substring(0, interval.length -1);
-    // }
-
-    // if ((timeLeft[interval] <= 0)) {
-    //   timerComponents.push(
-    //     <span className="countdown-text">
-    //       {"00"}{" "}
-    //     </span>
-    //   )
-    // } else {
-      timerComponents.push(
-
-        <span className="countdown-text">
-          {timeLeft[interval] < 10
-           ? ("0" + timeLeft[interval])
-            // ? (timeLeft[interval] > 0 ? "0" + timeLeft[interval] : "00")
-            : "" + timeLeft[interval]}{" "}
-            
-          {interval === "seconds" ? " " : ":"}
-        </span>
-      );
-    // }
-
-
-  });
-
   return (
     <div id="notification-bar">
       <div className="container">
         <div id="main-text">
-          <p>
-            <strong>
-              {timerComponents.length ? (
-                <>{timerComponents}&nbsp;<span className="countdown-text">to the Big Reveal!</span></>
-              ) : (
-                <a
-                  href="https://testnets.opensea.io/collection/uglyeyes"
-                  id="main-link"
-                  target={"_blank"}
-                >
-                  View Collection at Opensea.io
-                </a>
-              )}
-            </strong>
+          <p className="countdown-text">
+            <a href="https://testnets.opensea.io/collection/uglyeyes" id="main-link" target={"_blank"}>
+              View Collection at Opensea.io
+            </a>
           </p>
         </div>
       </div>
