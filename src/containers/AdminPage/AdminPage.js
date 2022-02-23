556167e47df02a9fa714727b655ee85c1546cf92
Ak <akalankapathirage22@gmail.com>
2022-02-23 12:40:13 +0530

    Admin Page Connect then Reveal button

--- src/containers/AdminPage/AdminPage.js
+++ src/containers/AdminPage/AdminPage.js
@@ -3,7 +3,7 @@ import isOwnerFunction from "../../backEndCalls/isOwner";
 import { useDispatch, useSelector } from "react-redux";
 import NotFound from "../NotFound";
 import { connect } from "../../redux/blockchain/blockchainActions";
-import { Screen } from "../../styles/globalStyles";
+import * as s from "../../styles/globalStyles";
 import "./AdminPage.css";
 import Modal from "../../components/Modal/Modal";
 
@@ -11,37 +11,79 @@ const AdminPage = () => {
   const dispatch = useDispatch();
   const blockchain = useSelector((state) => state.blockchain);
   const [isOwner, setIsOwner] = useState(false);
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     isOwnerFunction(blockchain, setIsOwner);
-    console.log("loggeed", isOwnerFunction(blockchain, setIsOwner), isOwner);
-  }, [isOwner, setIsOwner]);
+    console.log("isOwner", isOwner);
+  }, [isOwner, setIsOwner, blockchain.smartContract]);
 
   return (
-    <Screen>
+    <s.Screen>
       <>
-      <div id="container">
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            dispatch(connect());
-            setShowModal(true)
-          }}
-          className="button"
-        >
-          REVEAL COLLECTION
-        </button>
-        {showModal && <Modal setShowModal={setShowModal} />}
-      </div>
-      <div className="image-div">
-      <img src="config/images/logo.png" alt="Logo" className="responsive" width="600" height="400" />
-      </div>
+        <div id="container">
+          {blockchain.account === "" || blockchain.smartContract === null ? (
+            <>
+              <s.SpacerSmall />
 
+              <button
+                className="button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(connect(blockchain, setIsOwner));
+                }}
+              >
+                CONNECT
+              </button>
+
+              {blockchain.errorMsg !== "" ? (
+                <>
+                  <s.SpacerSmall />
+                  <s.TextDescription
+                    style={{
+                      textAlign: "center",
+                      color: "var(--accent-text)",
+                    }}
+                  >
+                    {blockchain.errorMsg}
+                  </s.TextDescription>
+                </>
+              ) : null}
+            </>
+          ) : (
+            <>
+
+              <s.SpacerMedium />
+              <s.SpacerSmall />
+              <s.Container ai={"center"} jc={"center"} fd={"row"}>
+                <button className="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowModal(true)
+                  }}
+                >
+                  REVEAL COLLECTION
+                </button>
+              </s.Container>
+            </>
+          )}
+
+          {showModal && <Modal setShowModal={setShowModal} blockchain={blockchain} />}
+        </div>
+
+        <div className="image-div">
+          <img
+            src="config/images/logo.png"
+            alt="Logo"
+            className="responsive"
+            width="600"
+            height="400"
+          />
+        </div>
       </>
-      {isOwner && <div>AdminPage</div>}
-    </Screen>
+    </s.Screen>
   );
 };
 
 export default AdminPage;
+
