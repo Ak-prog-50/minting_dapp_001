10d60646f8a6308d9affe66681a342722a78e055
Ak <akalankapathirage22@gmail.com>
2022-02-28 16:17:37 +0530

    SaleEndedComp

---
+++ src/components/SaleEndedComp/SaleEndedComp.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../../state/context/ApplicationContext/AppContextProvider";
+import {
+  TextTitle,
+  TextDescription,
+  SpacerSmall,
+} from "../../styles/globalStyles";
+import { StyledLink } from "../../styles/styledComponents";
+
+const SaleEndedComp = () => {
+  const { CONFIG } = useContext(AppContext);
+  return (
+    <>
+      <TextTitle style={{ textAlign: "center", color: "var(--accent-text)" }}>
+        The sale has ended.
+      </TextTitle>
+      <TextDescription
+        style={{ textAlign: "center", color: "var(--accent-text)" }}
+      >
+        You can still find {CONFIG.NFT_NAME} on
+      </TextDescription>
+      <SpacerSmall />
+      <StyledLink target={"_blank"} href={CONFIG.MARKETPLACE_LINK}>
+        {CONFIG.MARKETPLACE}
+      </StyledLink>
+    </>
+  );
+};
+
+export default SaleEndedComp;
