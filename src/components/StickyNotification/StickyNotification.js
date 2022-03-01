d15f801410aadcb0afc53d438962bc427af3aefe
Ak <akalankapathirage22@gmail.com>
2022-03-01 22:40:55 +0530

    removed hard coded marketplace link

--- src/components/StickyNotification/StickyNotification.js
+++ src/components/StickyNotification/StickyNotification.js
@@ -1,13 +1,16 @@
+import { useContext } from "react";
+import { AppContext } from "../../state/context/ApplicationContext/AppContextProvider";
 import "./stickyNotification.css";
 
 const StickyNotification = () => {
+  const {CONFIG} = useContext(AppContext)
   return (
     <div id="notification-bar">
       <div className="container">
         <div id="main-text">
           <p className="countdown-text">
-            <a href="https://testnets.opensea.io/collection/uglyeyes" id="main-link" target={"_blank"}>
-              View Collection at Opensea.io
+            <a href={CONFIG.MARKETPLACE_LINK} id="main-link" target={"_blank"}>
+              View Collection at {CONFIG.MARKETPLACE}
             </a>
           </p>
         </div>
