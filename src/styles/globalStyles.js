26d17fa3e1b8bd14850444cf5f7b06e5e3154bd4
Ak <akalankapathirage22@gmail.com>
2022-03-01 13:14:49 +0530

    textTitle + subTitle + description styles

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -59,20 +59,20 @@ export const Container = styled.div`
 
 export const TextTitle = styled.p`
   color: var(--primary-text);
-  font-size: 22px;
+  font-size: 31px;
   font-weight: 500;
   line-height: 1.6;
 `;
 
 export const TextSubTitle = styled.p`
   color: var(--primary-text);
-  font-size: 18px;
+  font-size: 25px;
   line-height: 1.6;
 `;
 
 export const TextDescription = styled.p`
   color: var(--primary-text);
-  font-size: 16px;
+  font-size: 21px;
   line-height: 1.6;
 `;
 
