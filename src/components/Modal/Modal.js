bb0ec3052093b491fea56b71564f32d11635c49f
Ak <akalankapathirage22@gmail.com>
2022-03-02 12:50:05 +0530

    Modal when already revealed

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,40 +1,57 @@
 import revealNFTs from "../../backEndCalls/revealNFTs";
+import checkRevealed from "../../backEndCalls/checkRevealed";
+import { SpacerSmall } from "../../styles/globalStyles";
 import "./modalStyles.css";
 
-const Modal = ({ setShowModal, blockchain }) => {
+const Modal = ({
+  setShowModal,
+  blockchain,
+  isRevealed,
+  setIsRevealed,
+  CONFIG,
+}) => {
   return (
     <>
       <div id="id01" className="modal">
-        <span
-          onClick={() => setShowModal(false)}
-          className="close"
-          title="Close Modal"
-        >
-          &times;
-        </span>
         <div className="modal-content">
           <div className="container">
-            <h1>Are you sure you want to reveal your NFT collection?</h1>
+            {!isRevealed ? (
+              <h1>Are you sure you want to reveal your NFT collection?</h1>
+            ) : (
+              <div className="isRevealedContainer">
+                <h1>You have already revealed</h1>
+                <SpacerSmall />
+                <span
+                  onClick={() => setShowModal(false)}
+                  className="close"
+                  title="Close Modal"
+                >
+                  &times;
+                </span>
+              </div>
+            )}
 
-            <div className="clearfix">
-              <button
-                type="button"
-                className="cancelbtn"
-                onClick={() => setShowModal(false)}
-              >
-                Cancel
-              </button>
-              <button
-                type="button"
-                className="deletebtn"
-                onClick={() => {
-                  setShowModal(false)
-                  revealNFTs( blockchain)
-                }}
-              >
-                Reveal
-              </button>
-            </div>
+            {!isRevealed && (
+              <div className="clearfix">
+                <button
+                  type="button"
+                  className="cancelbtn"
+                  onClick={() => setShowModal(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="deletebtn"
+                  onClick={() => {
+                    setShowModal(false);
+                    revealNFTs(blockchain);
+                  }}
+                >
+                  Reveal
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
